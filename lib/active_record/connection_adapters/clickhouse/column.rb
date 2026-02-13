2b3129a66c66bdc71938cc5b375bb26fd0dba810
Vladislav Syabruk <vlad@syabruk.com>
2026-02-13 12:30:26 +0300

    Remove useless cast_type methods from Column class

--- lib/active_record/connection_adapters/clickhouse/column.rb
+++ lib/active_record/connection_adapters/clickhouse/column.rb
@@ -10,41 +10,6 @@ module ActiveRecord
           @codec = codec
         end
 
-        def key_type
-          return nil unless type == :map
-
-          cast_type(map_types.first)
-        end
-
-        def value_type
-          return nil unless type == :map
-
-          cast_type(map_types.last)
-        end
-
-        private
-
-        def map_types
-          sql_type_metadata.sql_type.match(/Map\((.+)\,\s?(.+)\)/).captures
-        end
-
-        def cast_type(type)
-          return type if type.nil?
-
-          case type
-          when /U?Int\d+/
-            :integer
-          when /DateTime/
-            :datetime
-          when /Date/
-            :date
-          when /Array/
-            type
-          else
-            :string
-          end
-        end
-
         private
 
         def deduplicated
